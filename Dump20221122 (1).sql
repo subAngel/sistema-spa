c49c61ac7b9d9fa9dfc37d5c1f08c02ea7f81a67
SilvinaLizet <106893148+SilvinaLizet@users.noreply.github.com>
2022-11-22 22:46:14 -0600

    Add files via upload

---
+++ Dump20221122 (1).sql
@@ -0,0 +1,196 @@
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
+--
+-- Host: localhost    Database: spa_db1
+-- ------------------------------------------------------
+-- Server version	8.0.22
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `paciente`
+--
+
+DROP TABLE IF EXISTS `paciente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paciente` (
+  `id_paciente` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellidos` varchar(45) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `descuento` int DEFAULT NULL,
+  `condicion_salud` varchar(105) DEFAULT NULL,
+  PRIMARY KEY (`id_paciente`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paciente`
+--
+
+LOCK TABLES `paciente` WRITE;
+/*!40000 ALTER TABLE `paciente` DISABLE KEYS */;
+INSERT INTO `paciente` VALUES (2,'Angel Jesus','Zorrilla','whoangel.agl@gmail.com',10,'dolor de espalda'),(4,'Israel','Manzano Sanchez','isra@itoaxaca.edu.mx',20,'dolor de espalda');
+/*!40000 ALTER TABLE `paciente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `paciente_terapeuta`
+--
+
+DROP TABLE IF EXISTS `paciente_terapeuta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `paciente_terapeuta` (
+  `id_p_t` int NOT NULL AUTO_INCREMENT,
+  `id_paciente` int NOT NULL,
+  `id_terapeuta` int NOT NULL,
+  `id_servicio` int NOT NULL,
+  `total` int NOT NULL,
+  PRIMARY KEY (`id_p_t`),
+  KEY `fk_paciente_idx` (`id_paciente`),
+  KEY `fk_terapeuta_idx` (`id_terapeuta`),
+  KEY `fk_servicio_idx` (`id_servicio`),
+  CONSTRAINT `fk_paciente` FOREIGN KEY (`id_paciente`) REFERENCES `paciente` (`id_paciente`),
+  CONSTRAINT `fk_servicio` FOREIGN KEY (`id_servicio`) REFERENCES `servicios` (`id_servicios`),
+  CONSTRAINT `fk_terapeuta` FOREIGN KEY (`id_terapeuta`) REFERENCES `terapeuta` (`id_terapeuta`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `paciente_terapeuta`
+--
+
+LOCK TABLES `paciente_terapeuta` WRITE;
+/*!40000 ALTER TABLE `paciente_terapeuta` DISABLE KEYS */;
+INSERT INTO `paciente_terapeuta` VALUES (1,2,2,2,540),(2,2,2,4,720),(3,2,3,5,810),(4,4,3,4,640);
+/*!40000 ALTER TABLE `paciente_terapeuta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `reserva`
+--
+
+DROP TABLE IF EXISTS `reserva`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reserva` (
+  `id_reserva` int NOT NULL AUTO_INCREMENT,
+  `pk_paciente` int DEFAULT NULL,
+  `fecha` datetime DEFAULT NULL,
+  `status_pago` tinyint DEFAULT NULL,
+  PRIMARY KEY (`id_reserva`),
+  KEY `pk_paciente_idx` (`pk_paciente`),
+  CONSTRAINT `pk_paciente` FOREIGN KEY (`pk_paciente`) REFERENCES `paciente` (`id_paciente`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reserva`
+--
+
+LOCK TABLES `reserva` WRITE;
+/*!40000 ALTER TABLE `reserva` DISABLE KEYS */;
+INSERT INTO `reserva` VALUES (1,2,'2022-10-10 15:12:00',1),(2,2,'2022-12-31 15:30:21',0),(3,2,'2022-12-31 15:30:21',0),(4,2,'2022-12-31 15:30:21',0),(5,2,'2022-12-31 15:30:21',0),(6,4,'2022-12-31 15:30:21',0);
+/*!40000 ALTER TABLE `reserva` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `servicios`
+--
+
+DROP TABLE IF EXISTS `servicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servicios` (
+  `id_servicios` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `monto` int NOT NULL,
+  `descripcion` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id_servicios`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servicios`
+--
+
+LOCK TABLES `servicios` WRITE;
+/*!40000 ALTER TABLE `servicios` DISABLE KEYS */;
+INSERT INTO `servicios` VALUES (1,'paquete 1',500,'Descripcion del paquete 1'),(2,'paquete 2',600,'Descripcion del paquete 2'),(3,'paquete 3',700,'Descripcion del paquete 3'),(4,'paquete 4',800,'Descripcion del paquete 4'),(5,'paquete 5',900,'Descripcion del paquete 5'),(6,'paquete 6',1000,'Descripcion del paquete 6');
+/*!40000 ALTER TABLE `servicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `terapeuta`
+--
+
+DROP TABLE IF EXISTS `terapeuta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `terapeuta` (
+  `id_terapeuta` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellidos` varchar(45) NOT NULL,
+  `turno` varchar(20) NOT NULL,
+  `sueldo` int NOT NULL,
+  `especialidad` varchar(50) NOT NULL,
+  `cedula` varchar(13) NOT NULL,
+  PRIMARY KEY (`id_terapeuta`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `terapeuta`
+--
+
+LOCK TABLES `terapeuta` WRITE;
+/*!40000 ALTER TABLE `terapeuta` DISABLE KEYS */;
+INSERT INTO `terapeuta` VALUES (2,'Angel ','Cuevas','Matutino',8000,'espalda','1931948714'),(3,'Claudia','Arango','Vespertino',29921,'hombros','23232332');
+/*!40000 ALTER TABLE `terapeuta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `iduser` int NOT NULL AUTO_INCREMENT,
+  `user` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`iduser`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','root'),(2,'admin','root'),(3,'angel','angel');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-11-22 22:45:31
